b52e397810c3fab0a49c1f1f688e79dac419d833
Seth Mwebi <sethmwebi27@gmail.com>
2024-09-15 11:20:19 +0300

    sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', TRUE),
+('Rowdy', 8, 'M', 'Black', 'Aggressive', TRUE),
+('Tabitha', 6, 'F', 'Brown', 'Friendly', TRUE),
+('Sergeant Brown', 12, 'M', 'Brown', 'Stern', FALSE),
+('Melissa', 4, 'F', 'Black', 'Playful', TRUE),
+('Grinch', 9, 'M', 'Brown', 'Grumpy', TRUE),
+('Wendy', 7, 'F', 'Black', 'Gentle', TRUE),
+(NULL, 5, 'F', 'Brown', 'Shy', TRUE);
